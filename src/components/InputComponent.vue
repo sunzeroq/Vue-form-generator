502a3609502167e30d9244d3983ac6e3e5c63095
sunzeroq <596594483@qq.com>
2022-11-09 01:16:29 +0800

    transfer project

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,64 @@
+<template>
+    <div
+        class="form-input-container"
+    >
+        <div class="form-input-label"><span>{{ elementData.label }}：</span></div>
+        <div class="form-input-content">
+            <Input
+                v-if="elementData.disable"
+                disabled
+                :type="elementData.type"
+                v-bind="$attrs"
+                v-model="computedValue"
+                placeholder="请输入"
+            />
+            <Input
+                v-else
+                :type="elementData.type"
+                v-bind="$attrs"
+                v-model="computedValue"
+                placeholder="请输入"
+            />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'InputComponent',
+    props: ["elementData", "formData"],
+    data() {
+        return {
+            key: this.elementData.key
+        }
+    },
+    computed: {
+        computedValue: {
+            get() {
+                return this.formData[this.key];
+            },
+            set(val) {
+                let o = {
+                    key: this.key,
+                    value: val
+                }
+                this.$emit("input", o);
+            }
+        },
+    }
+};
+</script>
+<style lang='scss' scoped>
+.form-input-container {
+    .form-input-label {
+        span {
+            overflow: hidden;
+            text-overflow:ellipsis;
+            white-space: nowrap;
+            font-size: 14px;
+        }
+    }
+    .form-input-content {
+        margin-top: 4px;
+    }
+}
+</style>
